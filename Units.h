4bed2d7adbb5882f940443966553d4f23d80bda3
korvin001 <geller_misha@mail.ru>
2018-03-17 01:55:39 +0300

    Library of Units. World-Library is for starting the game. Working wersion of game with innoxious enemy bots. No pattern Builder.

---
+++ Units.h
@@ -0,0 +1,353 @@
+//
+// Created by Mike on 15.03.18.
+//
+
+#ifndef MYGAME_UNITS_H
+#define MYGAME_UNITS_H
+
+#include "Race.h"
+#include "Map.h"
+
+class IUnit {
+public:
+    IUnit() {}
+    IUnit(IRace* r, Map* map) {}
+    virtual void Move(int &mp)=0;
+    virtual void Damage(int &move, int x, int y)=0;
+
+    void TakeDamage(int damage) {
+        damage -=  defence + _race->getBonusDefence() + defenceWeaponLevel*5;
+        if (damage <= 0)
+            damage = 0;
+        if (damage > health)
+            damage = health;
+        if (damage) health -= damage;
+        cout << UnitName << " " << "lost " << damage << " hp." << endl;
+        if (health <= 0)
+            Death();
+    }
+    void Death() {
+        cout << UnitName << " has dead.";
+        (*army)[id] = NULL;
+        (*mask)[x - 1][y - 1] = NULL;
+        map->map[x - 1][y - 1] = '.';
+        map->secret[x - 1][y - 1] = '*';
+    };
+    void getInfo() {
+        cout << UnitName << endl;
+        cout << "Health: " << health << endl;
+        cout << "Attack: " << attack + _race->getBonusAttack();
+        (attackDistance > 1) ? cout << " Attack Distance: " << attackDistance << endl:cout << endl;
+        cout << "Defence: " << defence + _race->getBonusDefence() << endl;
+    }
+    string getName() { return UnitName; }
+    void takeOrders() {
+        int movepoints = move + _race->getBonusMove();
+        while (movepoints) {
+            getInfo();
+            cout << "Movepoints: " << movepoints << endl << endl;
+            cout << "Listen to your orders!" << endl;
+            cout << "1)Attack" << endl;
+            cout << "2)Move" << endl;
+            cout << "3)Nothing" << endl << endl;
+            char choice = '3';
+            cin >> choice;
+            if (choice == '1') {
+                cout << "Say the coordinares, my Lord!" << endl;
+                int cx, cy;
+                cin >> cx >> cy;
+                if ((*mask)[cx - 1][cy - 1])
+                   Damage(movepoints, cx, cy);
+                else {
+                    cout << "Wrong Information!" << endl << endl;
+                    break;
+                }
+            }
+            if (choice == '2')
+                Move(movepoints);
+            if (choice == '3')
+                break;
+        }
+    }
+    void look(){
+        int d = lookDistance + _race->getBonusLookDistance();
+        int i0 = max(1, x - d);
+        int j0 = max(1, y - d);
+        int il = min(map->getSize(), x + d);
+        int jl = min(map->getSize(), y + d);
+        for (int i = i0; i <= il; i++)
+            for (int j = j0; j <= jl; j++)
+                if (map->map[i - 1][j - 1] == '*')
+                    map->map[i - 1][j - 1] = map->secret[i - 1][j - 1] == '*' ? '.' : map->secret[i - 1][j - 1];
+    }
+    void unlook() {
+        int d = lookDistance + _race->getBonusLookDistance();
+        int i0 = max(1, x - d);
+        int j0 = max(1, y - d);
+        int il = min(map->getSize(), x + d);
+        int jl = min(map->getSize(), y + d);
+        for (int i = i0; i <= il; i++)
+            for (int j = j0; j <= jl; j++)
+                if (map->map[i - 1][j - 1] == '.' || !(map->map[i - 1][j - 1] >= 'A' && map->map[i - 1][j - 1] <='Z'))
+                    map->map[i - 1][j - 1] = '*';
+        for (int i = 0;i < (*army).size();i++)
+            if ((*army)[i] != this && (*army)[i])
+                (*army)[i]->look();
+    }
+    void setPlace(int x, int y) {
+        this->x = x;
+        this->y = y;
+        if (!isEnemy)
+          map->map[x-1][y-1] = symbol;
+        map->secret[x-1][y-1] = symbol;
+        if (!isEnemy)
+          look();
+    }
+    char getSymbol() { return symbol; }
+    int getX() {return x; }
+    int getY() {return y; }
+    void healing(){
+        if (health != maxhealth)
+            cout << UnitName << " is healing." << endl;
+        health = min(maxhealth, health + _race->getBonusHealth());
+    }
+    void setArmy(vector<IUnit*> *a) {
+        army = a;
+    }
+    void setMask(vector<vector<IUnit*>> *m) {
+        mask = m;
+    }
+
+protected:
+    IRace* _race;
+    vector<IUnit*> *army;
+    vector<vector<IUnit*>> *mask;
+    string UnitName;
+    int move;
+    int defence;
+    int attack;
+    int attackDistance = 1;
+    int lookDistance;
+    int health;
+    int maxhealth;
+    int attackWeaponLevel = 0;
+    int defenceWeaponLevel = 0;
+    int x;
+    int y;
+    char symbol;
+    Map* map;
+    bool isEnemy;
+    int id;
+};
+
+class Warrior : public IUnit {
+public:
+    Warrior() {}
+    Warrior(IRace* race, Map* m, bool ie) {
+        id = 2;
+        _race = race;
+        map = m;
+        UnitName = _race->getNameWarrior();
+        move = 6;
+        defence = 20;
+        attack = 250;
+        lookDistance = 4;
+        health = 600;
+        maxhealth = 600;
+        symbol = 'w';
+        isEnemy = ie;
+        if (!ie)
+            symbol += 'A' - 'a';
+    }
+    void Move(int &mp) override {
+        cout << "Say the string of moving includes 'u','d','r','l': ";
+        string s;
+        cin >> s;
+        if (s.length() > mp)
+            s = s.substr(0, mp);
+        unlook();
+        map->map[x-1][y-1] = '*';
+        map->secret[x-1][y-1] = '*';
+        (*mask)[x-1][y-1] = NULL;
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == 'r' && y < map->getSize() && map->secret[x - 1][y] == '*')
+                y++;
+            if (s[i] == 'l' && y > 1 && map->secret[x - 1][y - 2] == '*')
+                y--;
+            if (s[i] == 'u' && x > 1 && map->secret[x - 2][y - 1] == '*')
+                x--;
+            if (s[i] == 'd' && x < map->getSize() && map->secret[x][y - 1] == '*')
+                x++;
+        }
+        map->map[x-1][y-1] = symbol;
+        map->secret[x-1][y-1] = symbol;
+        (*mask)[x-1][y-1] = this;
+        look();
+        map->showMap();
+        mp = 0;
+    }
+
+    void Damage(int &move, int cx, int cy) {
+        Move(move);
+        if (abs(x - cx) + abs(y - cy) == 1) {
+            (*mask)[cx - 1][cy - 1]->TakeDamage(attack + _race->getBonusAttack() + attackWeaponLevel * 5);
+        }
+        else {
+            cout << "Sorry, it was a mistake!" << endl;
+            TakeDamage(45);
+        }
+        map->showMap();
+    }
+
+};
+
+class Archer : public IUnit {
+public:
+    Archer() {}
+    Archer(IRace* race, Map* m, bool ie) {
+        _race = race;
+        map = m;
+        UnitName = _race->getNameArcher();
+        move = 6;
+        defence = 10;
+        attack = 120;
+        lookDistance = 5;
+        health = 400;
+        symbol = 'a';
+        isEnemy = ie;
+        maxhealth = 400;
+        attackDistance = 5;
+        id = 1;
+        if (!ie)
+            symbol += 'A' - 'a';
+    }
+    void Move(int &mp) override {
+        cout << "Say the string of moving includes 'u','d','r','l': ";
+        string s;
+        cin >> s;
+        if (s.length() > mp)
+            s = s.substr(0, mp);
+        unlook();
+        map->map[x-1][y-1] = '*';
+        map->secret[x-1][y-1] = '*';
+        (*mask)[x-1][y-1] = NULL;
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == 'r' && y < map->getSize() && map->secret[x - 1][y] == '*')
+                y++;
+            if (s[i] == 'l' && y > 1 && map->secret[x - 1][y - 2] == '*')
+                y--;
+            if (s[i] == 'u' && x > 1 && map->secret[x - 2][y - 1] == '*')
+                x--;
+            if (s[i] == 'd' && x < map->getSize() && map->secret[x][y - 1] == '*')
+                x++;
+        }
+        map->map[x-1][y-1] = symbol;
+        map->secret[x-1][y-1] = symbol;
+        (*mask)[x-1][y-1] = this;
+        look();
+        map->showMap();
+        mp = 0;
+    }
+
+    void Damage(int &move, int cx, int cy) override {
+        if (move < 3) {
+            cout << "Sorry, it was a mistake!" << endl;
+            return;
+        }
+
+        if (abs(x - cx) + abs(y - cy) <= attackDistance + _race->getBonusArcherDistance()) {
+            (*mask)[cx - 1][cy - 1]->TakeDamage(attack + _race->getBonusAttack() + attackWeaponLevel * 5);
+        }
+        else {
+            cout << "Sorry, it was a mistake!" << endl;
+            TakeDamage(45);
+        }
+        move = max(0, move - 3);
+        map->showMap();
+    }
+
+
+};
+
+class Spy : public IUnit {
+public:
+    Spy() {}
+    Spy(IRace* race, Map* m, bool ie) {
+        _race = race;
+        map = m;
+        UnitName = _race->getNameSpy();
+        move = 8;
+        defence = 20;
+        attack = 90;
+        lookDistance = 7;
+        health = 300;
+        maxhealth = 300;
+        symbol = 's';
+        isEnemy = ie;
+        id = 0;
+        if (!ie)
+            symbol += 'A' - 'a';
+    }
+    void Move(int &mp) override {
+        cout << "Say the strings of moving includes 'u','d','r','l' with '!' after the last: ";
+        int d = mp;
+        map->map[x-1][y-1] = '*';
+        char t;
+        string s="";
+        int j = 0;
+        for (int i = 0; i < d; i++) {
+            map->map[x-1][y-1] = '*';
+            map->secret[x-1][y-1] = '*';
+            (*mask)[x-1][y-1] = NULL;
+            unlook();
+            mp--;
+            if (j == s.length()) {
+                j = 0;
+                cin >> s;
+            }
+            if (s[j] == 'r' && y < map->getSize() && map->secret[x - 1][y] == '*')
+                y++;
+            if (s[j] == 'l' && y > 1 && map->secret[x - 1][y - 2] == '*')
+                y--;
+            if (s[j] == 'u' && x > 1 && map->secret[x - 2][y - 1] == '*')
+                x--;
+            if (s[j] == 'd' && x < map->getSize() && map->secret[x][y - 1] == '*')
+                x++;
+            map->map[x - 1][y - 1] = symbol;
+            map->secret[x-1][y-1] = symbol;
+            (*mask)[x-1][y-1] = this;
+            look();
+            if (s[j] == '!') {
+                mp++;
+                map->showMap();
+                return;
+            }
+            j++;
+            if (j == s.length() || i == d - 1) {
+                cout << endl << endl;
+                map->showMap();
+            }
+        }
+    }
+
+    void Damage(int &move, int cx, int cy) {
+        move -= 2;
+        if (move > 0) {
+            Move(move);
+        }
+        if (abs(x - cx) + abs(y - cy) == 1) {
+            (*mask)[cx - 1][cy - 1]->TakeDamage(attack + _race->getBonusAttack() + attackWeaponLevel * 5);
+        }
+        else
+            TakeDamage(45);
+        map->showMap();
+    }
+
+
+
+};
+
+
+
+
+#endif //MYGAME_UNITS_H
---
+++ World.h
@@ -0,0 +1,140 @@
+//
+// Created by Mike on 15.03.18.
+//
+
+#ifndef MYGAME_WORLD_H
+#define MYGAME_WORLD_H
+
+#include "Units.h"
+
+class World {
+public:
+    Map *m;
+    IRace* myRace;
+    IRace* enemyRace;
+    vector<vector<IUnit*>> mask;
+    vector<IUnit*> myArmy;
+    vector<IUnit*> enemyArmy;
+
+    World(){
+        m = new Map();
+        myRace = new Humans();
+        enemyRace = new Dwarfes();
+        mask.resize(m->getSize());
+        for (int i = 0;i < mask.size();i++)
+            for (int j = 0; j < mask.size();j++)
+                mask[i].push_back(NULL);
+        enemyArmy.resize(3);
+        enemyArmy[0] = new Spy(enemyRace, m, 1);
+        enemyArmy[1] = new Archer(enemyRace, m, 1);
+        enemyArmy[2] = new Warrior(enemyRace, m, 1);
+        enemyArmy[0]->setArmy(&enemyArmy);
+        enemyArmy[1]->setArmy(&enemyArmy);
+        enemyArmy[2]->setArmy(&enemyArmy);
+        enemyArmy[0]->setPlace(15, 13);
+        enemyArmy[1]->setPlace(25, 4);
+        enemyArmy[2]->setPlace(5, 25);
+        mask[14][12] = enemyArmy[0];
+        mask[24][3] = enemyArmy[1];
+        mask[4][24] = enemyArmy[2];
+
+        myArmy.resize(3);
+        myArmy[0] = new Spy(myRace, m, 0);
+        myArmy[1] = new Archer(myRace, m, 0);
+        myArmy[2] = new Warrior(myRace, m, 0);
+        myArmy[0]->setArmy(&myArmy);
+        myArmy[1]->setArmy(&myArmy);
+        myArmy[2]->setArmy(&myArmy);
+        myArmy[0]->setPlace(1, 3);
+        myArmy[1]->setPlace(1, 2);
+        myArmy[2]->setPlace(1, 1);
+        mask[0][2] = myArmy[0];
+        mask[0][1] = myArmy[1];
+        mask[0][0] = myArmy[2];
+
+        enemyArmy[0]->setMask(&mask);
+        enemyArmy[1]->setMask(&mask);
+        enemyArmy[2]->setMask(&mask);
+        myArmy[0]->setMask(&mask);
+        myArmy[1]->setMask(&mask);
+        myArmy[2]->setMask(&mask);
+
+    }
+
+    void healing() {
+        for (int i = 0; i < myArmy.size(); i++) {
+            if (myArmy[i])
+                myArmy[i]->healing();
+        }
+        for (int i = 0; i < myArmy.size(); i++) {
+            if (enemyArmy[i])
+                enemyArmy[i]->healing();
+        }
+    }
+
+    void war() {
+        bool win = false;
+        bool lose = false;
+        int step = 0;
+        while(!win && !lose) {
+            m->showMap();
+            step++;
+            cout << endl << "STEP " << " " << step << endl;
+            win = true;
+            lose = true;
+            if (myArmy[0]) {
+                myArmy[0]->takeOrders();
+                lose = false;
+            }
+            if (myArmy[1]) {
+                myArmy[1]->takeOrders();
+                lose = false;
+            }
+            if (myArmy[2]) {
+                myArmy[2]->takeOrders();
+                lose = false;
+            }
+
+            healing();
+
+            if (enemyArmy[0]) {
+                cout << "Enemy:" << endl;
+                enemyArmy[0]->getInfo();
+                cout << endl;
+                win = false;
+            }
+            if (enemyArmy[1]) {
+                cout << "Enemy:" << endl;
+                enemyArmy[1]->getInfo();
+                cout << endl;
+                win = false;
+            }
+            if (enemyArmy[2]) {
+                cout << "Enemy:" << endl;
+                enemyArmy[2]->getInfo();
+                cout << endl;
+                win = false;
+            }
+        }
+        if (win) cout << "YOU WIN!" << endl;
+        if (lose) cout << "YOU LOSE!" << endl;
+    }
+
+};
+
+void check(vector<vector<IUnit*>> &mask) {
+    for (int i = 0; i < 10; i++)
+        for (int j = 0; j < 10;j++)
+            if (mask[i][j])
+                cout << i << " " << j << " " <<  mask[i][j]->getName() << endl;
+}
+
+void makeWorld() {
+    World *world = new World();
+    world->war();
+
+}
+
+
+
+#endif //MYGAME_WORLD_H
